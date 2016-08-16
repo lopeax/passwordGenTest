20cd59d55460c42d66c7768a0be7396075880f4e
Tony Lopez <tony@alberon.co.uk>
2016-08-16 10:38:41 +0100

    Added password generator to test

--- www/index.php
+++ www/index.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello World!</div>
+                <div class="title">Password Generator</div>
             </div>
         </div>
     </body>
